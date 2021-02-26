0c644d7b422d51d21283cd97cd2b4abb07c62b54
Vikas Ramnani <vikasraamnani@gmail.com>
2021-02-26 14:53:46 +0100

    delete

--- realm-monitoring/functions/insertMonitoringData/source.js
+++
@@ -1,5 +0,0 @@
-exports = function(database, collection, measurements){
-    var collection = context.services.get("mongodb-atlas").db(database).collection(collection);
-    var doc = collection.insertMany(measurements);
-
-};
